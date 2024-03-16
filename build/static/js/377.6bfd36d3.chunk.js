37b16edf2822d8ad4e11106f472c5c5a316c5527
Rishad <rishad.pk@domainenroll.com>
2024-03-16 13:15:34 +0530

    removed build folder from git

--- build/static/js/377.6bfd36d3.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkdevlacus=self.webpackChunkdevlacus||[]).push([[377],{85377:function(e,u,s){s.r(u);var a=s(50845);u.default=a.g}}]);
-//# sourceMappingURL=377.6bfd36d3.chunk.js.map
